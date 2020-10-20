e90342fdffb24b0bbbc9423305877dc429ea1e75
tonyng <tonyng987@gmail.com>
2020-10-19 19:43:30 -0700

    Demo snapshot testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,6 @@
 import React from "react";
+// import "jest-enzyme";
+import toJSON from "enzyme-to-json";
 import { shallow, configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import App from "./App";
@@ -7,4 +9,9 @@ configure({ adapter: new Adapter() });
 
 it("renders without crashing", () => {
   shallow(<App />);
+});
+
+it("renders correctly", () => {
+  const tree = shallow(<App />);
+  expect(toJSON(tree)).toMatchSnapshot();
 });
