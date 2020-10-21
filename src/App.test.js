f2de7833839923b1faea1c484f79f0cf479cf879
tonyng <tonyng987@gmail.com>
2020-10-20 19:45:42 -0700

    Update tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,9 @@
 import React from "react";
-// import toJSON from "enzyme-to-json";
 import { shallow } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
 import App from "./App";
 import Shop from "./shop/Shop";
 
-// configure({ adapter: new Adapter() });
-
-it("renders without crashing", () => {
-  shallow(<App />);
-});
+const clickFn = jest.fn();
 
 it("renders correctly", () => {
   const tree = shallow(<App />);
@@ -20,5 +14,28 @@ describe("Shop", () => {
   it('renders correctly', () => {
     const shop = shallow(<Shop />);
     expect(shop).toMatchSnapshot();
+  });
+
+  it.skip("should render the correct items names", () => {
+    const wrapper = shallow(
+      <div>
+        <div>Book</div>
+        <div>Music CD</div>
+        <div>Chocolate bar</div>
+        <div>Imported chocolates</div>
+        <div>Perfume</div>
+        <div>Imported Perfume</div>
+        <div>Headache pills</div>
+      </div>
+    );
+
+    const texts = wrapper.find().map((node) => node.text());
+    expect(texts).to.eql(["Book", "Music CD", "Chocolate bar"]);
+  })
+
+  it("button click should add item to cart", () => {
+    const component = shallow(<Shop onClick={clickFn} />);
+    component.find("button").at(0).simulate("click");
+    expect(clickFn).toHaveBeenCalled();
   });
 });
